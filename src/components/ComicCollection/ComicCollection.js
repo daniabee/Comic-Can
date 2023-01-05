be7679297b8382e9e1ca29b63488b75b1825acda
Dani Bagley <bagley.a.danielle@gmail.com>
2023-01-04 19:58:44 -0700

    Add comic card

--- src/components/ComicCollection/ComicCollection.js
+++ src/components/ComicCollection/ComicCollection.js
@@ -1,7 +1,13 @@
 import React from "react";
+import ComicCard from "../ComicCard/ComicCard";
 
 const ComicCollection = () => {
+  // const comicCards = comics.map(item => {
+  //   return < ComicCard />
+  // })
+
   return <p>HIIIIIIIII</p>;
+  //{comicCards}
 };
 
 export default ComicCollection;
---
+++ src/components/ComicDetail/ComicDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ComicDetail = () => {
+  return (
+    <div>
+      <p>info</p>
+      <p>info</p>
+      <p>info</p>
+      <p>info</p>
+    </div>
+  );
+};
