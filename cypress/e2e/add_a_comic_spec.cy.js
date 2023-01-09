1d2174707782045968dab5c66a1ede036e235611
Eleanor Ruth <ellie.ruth@me.com>
2023-01-09 11:32:59 -0700

    Add test for form

--- cypress/e2e/add_a_comic_spec.cy.js
+++ cypress/e2e/add_a_comic_spec.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Add A Comic Form', () => {
+  beforeEach(()=> {
+    cy.visit('https://stretch-project-iota.vercel.app/')
+    cy.get('.add-comic-link').click()
+  })
+  it('Should have a form to add a comic', () => {
+    cy.get('.form').should('be.visible')
+  })
+  it('Should have a title input field ', () => {
+    cy.get(':nth-child(1) > label').contains('Title')
+    cy.get(':nth-child(1) > label > input').should('have.value', '')
+  })
+  it('Should have a year input field ', () => {
+    cy.get(':nth-child(2) > label').contains('Year')
+    cy.get(':nth-child(2) > label > input').should('have.value', '')
   })
 })
