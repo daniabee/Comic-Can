70ff194ef0d6a76a06f43045a9ece2c99dd6b4ff
Eleanor Ruth <ellie.ruth@me.com>
2023-01-08 17:55:55 -0700

    Add tests for main page

---
+++ cypress/e2e/main_comic_page.cy.js
@@ -0,0 +1,19 @@
+describe('Comic Can Home Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+  it('should have banner image', () => {
+    cy.get('.banner').should('have.attr', 'alt', "Comic Can")
+  })
+  it('should have a nav bar', () => {
+    cy.get('.navbar').should('exist')
+  })
+  it('should have a my collection button', () => {
+    cy.get('.my-collection-link').click()
+    cy.url().should('eq', 'http://localhost:3000/comicCollection')
+  })
+  it('should have an add a comic link', () => {
+    cy.get('.add-comic-link').click()
+    cy.url().should('eq', 'http://localhost:3000/addComic')
+  })
+})
